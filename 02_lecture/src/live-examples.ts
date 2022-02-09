2c6d14333025bde3ced488533bedfd45b7357e63
Arkady Zelensky <arkady.zelensky@yalantis.net>
2022-02-09 21:38:15 +0200

    added examples

--- 02_lecture/src/live-examples.ts
+++ 02_lecture/src/live-examples.ts
@@ -1,3 +1,29 @@
+type Nullable<T> = {
+  [P in keyof T]: T[P] extends Array<infer U> ? U[] : T[P] | null;
+};
+
+type Draft<
+  TDataStructure extends {},
+  TRequiredFields extends keyof TDataStructure,
+> = Pick<TDataStructure, TRequiredFields> &
+  Nullable<Omit<TDataStructure, TRequiredFields>>;
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+type PartialUser = Draft<User, 'id'>;
+
+let a: PartialUser = {
+  id: 1,
+  email: 'some@email.com',
+  name: 'Some',
+};
+console.log(a);
+// ----------------------
+
 interface UserEntity {
   id: string;
   name: string;
@@ -5,10 +31,10 @@ interface UserEntity {
 }
 
 type UserUpdateData = Omit<UserEntity, 'id'>;
-
 const dto: UserUpdateData = {
   name: 'New name',
   email: 'new.email@email.com',
+  // id: 2,
 };
 
 // ---------------------------------
@@ -45,7 +71,7 @@ class Lamp {
   constructor(private energySource: IEnergySource) {}
 
   public on(): void {
-    console.log(`Got energy from ${this.energySource.getEnergy().toUpperCase()}`);
+    console.log(`Got energy from ${this.energySource.getEnergy()}`);
   }
 }
 
